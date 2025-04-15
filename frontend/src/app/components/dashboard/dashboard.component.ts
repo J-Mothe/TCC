9cd8eb19b4fc52183b5d408acca9b3a8235bff3c
Joao Vantil <joao_mothe@hotmail.com>
2025-04-15 17:52:05 -0300

    Teste uptade angular

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, CardModule, TableModule],
 })
 export class DashboardComponent implements OnInit {
   dataRecords: any[] = [];
